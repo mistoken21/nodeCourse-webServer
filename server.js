d53a2dfd71d73819a59cca46a7d9676d545ccd57
Aditya <budholiya21@gmail.com>
2018-04-17 16:50:58 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + "/views/partials");
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+    fs.appendFileSync('server.log', log + "\n");
+    next();
+})
+app.use((req, res, next)=>{
+    res.render("maintainence.hbs")
+})
+app.use(express.static(__dirname + "/public"));
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle : "home Page",
+        message : "Welcome to the website"
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle : "About Page"
+    })
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage : "Something bad happened"
+    });
+})
+
+app.listen(3000);
